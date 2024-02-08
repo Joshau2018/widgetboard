becd26d434f0e112449a4d2070e763b4aff0d756
Joshau2018 <113706963+Joshau2018@users.noreply.github.com>
2024-02-08 14:43:32 -0600

    Update something.py

--- something.py
+++ something.py
@@ -17,5 +17,22 @@ a_textbox.focus()
 a_textbox.insert(index=tk.END, string='Some text to take up space')
 a_textbox.pack(pady=(5, 5))  # pady(x, y), x=Horizontal, y=Vert
 
+
+# Button
+def button_clicked():
+    a_label["text"] = f'{a_textbox.get()}'
+
+
+button = tk.Button(text="DO NOT CLICK", command=button_clicked)
+button.pack(pady=(0, 5))
+
+
+# Text (Multi-line textbox)
+text = tk.Text(width=30, height=5)
+# Add some text to begin with
+text.insert(index=tk.END, chars="Ex of multi line entry :)")
+text.pack(pady=(0, 5))
+
+
 # must be at the end
 window.mainloop()
